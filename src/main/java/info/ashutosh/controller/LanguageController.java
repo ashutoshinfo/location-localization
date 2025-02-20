2240dde7a00ea37952b1b2e6afcf477cf54c51aa
Ashutosh <69671369+ashutoshinfo@users.noreply.github.com>
2025-02-20 18:47:52 +0530

    Update LanguageController.java

--- src/main/java/info/ashutosh/controller/LanguageController.java
+++ src/main/java/info/ashutosh/controller/LanguageController.java
@@ -1,22 +1,37 @@
 package info.ashutosh.controller;
 
+import info.ashutosh.domain.entity.person.Language;
 import info.ashutosh.domain.json.common.CustomResponse;
+import info.ashutosh.dtos.CityDTO;
+import info.ashutosh.dtos.CountryDTO;
+import info.ashutosh.dtos.LanguageDTO;
+import info.ashutosh.dtos.StateDTO;
 import info.ashutosh.service.LanguageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/language")
 public class LanguageController {
 
-	@Autowired
-	LanguageService languageService;
+    @Autowired
+    LanguageService languageService;
 
-	@GetMapping(value = "/get")
-	public CustomResponse getLanguages() {
-		return languageService.getLanguages();
-	}
+    @GetMapping(value = "/get")
+    public CustomResponse getLanguages() {
+        return languageService.getLanguages();
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<LanguageDTO>> getAllLanguages() {
+        List<LanguageDTO> languages = languageService.getAllLanguages();
+        return ResponseEntity.ok(languages);
+    }
 
 }
