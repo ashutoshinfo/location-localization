90266e82fc08ece464b58ba64d2f6e8f26dc6870
Ashutosh <69671369+ashutoshinfo@users.noreply.github.com>
2025-02-20 14:00:59 +0530

    Update LaguageRepository.java

--- src/main/java/info/ashutosh/repository/LaguageRepository.java
+++ src/main/java/info/ashutosh/repository/LaguageRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import info.ashutosh.domain.entity.person.Language;
 
-public interface LaguageRepository extends JpaRepository<Language, Long> {
+public interface LanguageRepository extends JpaRepository<Language, Long> {
 
 }
